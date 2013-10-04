05f94936e5559224182a25a697263c77b15bbb2b
Premasagar Rose <p@dharmafly.com>
2013-10-04 17:50:59 +0100

    Update moves

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
+var df = require('dateformat');
 var autonomy = require('ardrone-autonomy');
+var arDrone = require('ar-drone');
+var arDroneConstants = require('ar-drone/lib/constants');
 var mission  = autonomy.createMission();
+
+function navdata_option_mask(c) {
+  return 1 << c;
+}
+
+// From the SDK.
+var navdata_options = (
+    navdata_option_mask(arDroneConstants.options.DEMO)
+  | navdata_option_mask(arDroneConstants.options.VISION_DETECT)
+  | navdata_option_mask(arDroneConstants.options.MAGNETO)
+  | navdata_option_mask(arDroneConstants.options.WIFI)
+);
+
+// Land on ctrl-c
+var exiting = false;
+process.on('SIGINT', function() {
+    if (exiting) {
+        process.exit(0);
+    } else {
+        console.log('Got SIGINT. Landing, press Control-C again to force exit.');
+        exiting = true;
+        mission.control().disable();
+        mission.client().land(function() {
+            process.exit(0);
+        });
+    }
+});
+
+// Connect and configure the drone
+mission.client().config('general:navdata_demo', true);
+mission.client().config('general:navdata_options', navdata_options);
+mission.client().config('video:video_channel', 1);
+mission.client().config('detect:detect_type', 12);
+
+// Log mission for debugging purposes
+mission.log("mission-" + df(new Date(), "yyyy-mm-dd_hh-MM-ss") + ".txt");
+
+
+/////
+
 
 var controller = {
     start: function(altitude){
         mission.takeoff()
            .zero()               // Sets the current state as the reference
-           .altitude(altitude);  // Climb to altitude in metres
+           .altitude(2);  // Climb to altitude in metres
+        
+        //mission.client().animate('flipLeft', 15);
 
         return this;
     },
@@ -41,12 +86,17 @@ var svg = {
             z: this.ALTITUDE_GO,
             yaw: 0
         });
+        mission.hover(1000);
 
         mission.altitude(this.ALTITUDE_DRAW)
                .right(attr.width)
+               .cw(90)
                .forward(attr.height)
+               .cw(90)
                .left(attr.width)
-               .backward(attr.height);
+               .cw(90)
+               .backward(attr.height)
+               .cw(90);
 
         return this;
     },
@@ -64,6 +114,7 @@ var svg = {
             z: this.ALTITUDE_GO,
             yaw: 0
         });
+        mission.hover(1000);
 
         mission.go({
             x: attr.x2,
@@ -96,18 +147,18 @@ var svg = {
 
 function init(){
     controller.start();
-    
+
     svg.rect({
             x: 1,
             y: 1,
             width: 2,
-            height: 3
+            height: 2
         })
         .line({
             x1: 0,
             y1: 0,
-            x2: 3,
-            y2: 2
+            x2: 2,
+            y2: 1
         });
 
     controller.end();
