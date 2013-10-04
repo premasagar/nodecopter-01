5f50c0d3da3d24802fcec4d69ee82d4938b596df
Premasagar Rose <p@dharmafly.com>
2013-10-04 15:41:40 +0100

    Functional rect()

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-var arDrone = require('ar-drone');
-var client = arDrone.createClient();
-
-client.disableEmergency();
-
 var autonomy = require('ardrone-autonomy');
 var mission  = autonomy.createMission();
 
-//function test(){
-    mission.takeoff()
+
+function test(){
+    var m = mission.takeoff()
            .zero()       // Sets the current state as the reference
-           .altitude(1)  // Climb to altitude = 1 meter
-           .forward(2)   
-           .right(2)     
-           .backward(2) 
-           .left(2)
-           .hover(1000)  // Hover in place for 1 second
+           .altitude(2)  // Climb to altitude = 1 meter
+           .forward(1)   
+           .right(1)     
+           .backward(1) 
+           .left(1)
+           //.hover(1000)  // Hover in place for 1 second
            .land();
-//}
+}
 
 function start(altitude){
     console.log("start");
@@ -28,6 +24,17 @@ function start(altitude){
 function end(){
     console.log("end");
     mission.land();
+
+    mission.run(function (err, result) {
+        if (err) {
+            console.trace("Oops, something bad happened: %s", err.message);
+            mission.client().stop();
+            mission.client().land();
+        } else {
+            console.log("Mission success!");
+            process.exit(0);
+        }
+    });
 }
 
 function rect(x, y, width, height){
@@ -44,16 +51,5 @@ function init(){
     rect(1, 1, 3, 2);
     end();
 }
-
-mission.run(function (err, result) {
-    if (err) {
-        console.trace("Oops, something bad happened: %s", err.message);
-        mission.client().stop();
-        mission.client().land();
-    } else {
-        console.log("Mission success!");
-        process.exit(0);
-    }
-});
 
 init();
