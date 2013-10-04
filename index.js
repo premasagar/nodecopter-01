283e635d7d61f2df503db7c358a27890f2f1d03b
Premasagar Rose <p@dharmafly.com>
2013-10-04 16:04:19 +0100

    Add geometry object

--- index.js
+++ index.js
@@ -1,9 +1,60 @@
 var autonomy = require('ardrone-autonomy');
 var mission  = autonomy.createMission();
 
+var geometry = {
+    start: function(altitude){
+        mission.takeoff()
+           .zero()               // Sets the current state as the reference
+           .altitude(altitude);  // Climb to altitude = 2 metres
 
-function test(){
-    var m = mission.takeoff()
+        return this;
+    },
+
+    end: function(){
+        mission.land()
+            .run(function (err, result) {
+                if (err) {
+                    console.trace("Oops, something bad happened: %s", err.message);
+                    mission.client().stop();
+                    mission.client().land();
+                } else {
+                    console.log("Mission success!");
+                    process.exit(0);
+                }
+            });
+
+        return this;
+    },
+
+    // x, y, width, height
+    rect: function(attr){
+        mission.go({
+            x: attr.x,
+            y: attr.y,
+            z:0,
+            yaw:0
+        });
+
+        mission.right(attr.width)
+               .forward(attr.height)
+               .left(attr.width)
+               .backward(attr.height);
+
+        return this;
+    },
+
+    // cx, cy, r
+    circle: function(attr){
+
+    },
+
+    // x1, y1, x2, y2
+    line: function(attr){
+
+    },
+
+    test: function(){
+        var m = mission.takeoff()
            .zero()       // Sets the current state as the reference
            .altitude(2)  // Climb to altitude = 1 meter
            .forward(1)   
@@ -12,44 +63,20 @@ function test(){
            .left(1)
            //.hover(1000)  // Hover in place for 1 second
            .land();
-}
 
-function start(altitude){
-    console.log("start");
-    mission.takeoff()
-           .zero()               // Sets the current state as the reference
-           .altitude(altitude);  // Climb to altitude = 2 metres
-}
+        return this;
+    }
+};
 
-function end(){
-    console.log("end");
-    mission.land();
 
-    mission.run(function (err, result) {
-        if (err) {
-            console.trace("Oops, something bad happened: %s", err.message);
-            mission.client().stop();
-            mission.client().land();
-        } else {
-            console.log("Mission success!");
-            process.exit(0);
-        }
-    });
-}
+/////
 
-function rect(x, y, width, height){
-    console.log("rect");
-    mission.go({x:x, y:y, z:0, yaw:0})
-           .right(width)
-           .forward(height)
-           .left(width)
-           .backward(height);
-}
 
 function init(){
-    start(2);
-    rect(1, 1, 3, 2);
-    end();
+    geometry
+        .start(3)
+        .rect(0.5, 0.5, 1.5, 1)
+        .end();
 }
 
 init();
